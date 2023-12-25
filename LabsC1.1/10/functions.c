29ebbb169e552c3c728158d85a7cc4266cb2f868
Phosphorus <necromeister112@gmail.com>
2023-12-25 15:45:35 +0300

    Final version

--- LabsC1.1/10/functions.c
+++ LabsC1.1/10/functions.c
@@ -3,15 +3,16 @@
 #include "ctype.h"
 #include "string.h"
 
-struct Worker {
+struct Worker 
+{
 	char name[30];
 	char famName[30];
 	char surName[30];
 	char post[30];
-	char year;
-	};
+	int year;
+};
 
-void checkingString(char *arg, char workers)
+void checkingString(char *arg, char *workers)
 {
     char text[20];
     fflush(stdin);
@@ -23,39 +24,14 @@ void checkingString(char *arg, char workers)
         {
             while ( sscanf(text, "%d", &number) == 1 )
             {
-                printf("\n(Error!) You've wrote not a string! \n", text);
+                printf("\n\033[0;31m(Error!) You've wrote not a string!\033[0m\n", text);
                 fflush(stdin);
                 fputs("Try again: ", stdout);
                 fgets(text, sizeof text, stdin);
             }   
-            printf("\n");
-        }
-        else
-        {
         }
     }
-}
-
-void checkingNumber(char *arg, char workers)
-{
-    char text[20];
-    fflush(stdin);
-    fputs(" ", stdout);
-    if ( fgets(text, sizeof text, stdin) ) 
-    {
-        int number;
-        if (sscanf(text, "%d", &number) != 1) 
-        {
-            while (sscanf(text, "%d", &number) != 1)
-            {
-                printf("\n(Error!) You've wrote not a number! \n", text);
-                fflush(stdin);
-                fputs("Try again: ", stdout);
-                fgets(text, sizeof text, stdin);
-            }   
-            printf("\n");   
-        }
-    }
+    strcpy(workers, text);
 }
 
 void inputWorker(struct Worker *workers, int n) 
@@ -66,37 +42,43 @@ void inputWorker(struct Worker *workers, int n)
         printf("\n\033[0;32mInfo about worker %d:\033[0m\n", i + 1);
         printf("First name:");
         strcpy(arg, "first name");
-        checkingString(arg, *workers[i].name);
-        workers[i].name =
+        checkingString(arg, workers[i].name);
 
         printf("\nFamily name:");
         strcpy(arg, "family name");
-        checkingString(arg, *workers[i].famName);
+        checkingString(arg, workers[i].famName);
 
-        printf("\nFamily name:");
+        printf("\nSurname:");
         strcpy(arg, "surname");
-        checkingString(arg, *workers[i].surName);
+        checkingString(arg, workers[i].surName);
 
         printf("\nPost:");
         strcpy(arg, "post");
-        checkingString(arg, *workers[i].post);
+        checkingString(arg, workers[i].post);
 
-        printf("\nYear:");
-        strcpy(arg, "post");
-        checkingNumber(arg, workers[i].year);
+        printf("\nYear: ");
+        while (scanf("%d", &workers[i].year) != 1) 
+        {
+            printf("\n\033[0;31m(Error!) You've wrote not a number!\033[0m\n");
+            fflush(stdin);
+            printf("Try again: ");
+        }
     }
 }
 
 void display(struct Worker *workers, int n, int k) 
 {
     printf("\n\033[0;32mWorker(s) with '%d and more' year(s) of job experience here:\033[0m\n", k);
-    for (int i = 0; i < n; i++) {
-        if ((int)workers[i].year >= k) {
-            printf("First name: %s,\nFamily Name: %s,\n" 
-           	"Surname: %s,\nPost: %s,\nYear: %s.\n",
+
+    for (int i = 0; i < n; i++) 
+    {
+        if (workers[i].year >= k) 
+        {   
+            printf("First name: %sFamily Name: %s" 
+           	"Surname: %sPost: %sYear: %d",
             workers[i].name, workers[i].famName, workers[i].surName,
-            workers[i].post, &workers[i].year);
-            printf("\n");
+            workers[i].post, workers[i].year);
         }
     }
 }
+
